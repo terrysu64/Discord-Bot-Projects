d8266a36e64e556cf767269c9a5c413f99153079
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-07-07 10:59:49 -0400

    Add files via upload

---
+++ JukeBot/JukeBot.py
@@ -0,0 +1,7 @@
+#Date: July 10, 2021
+#Author: Terry Su
+#Purpose: A free multi-functioning music streaming bot for members to use in a server;
+#         start by using $help and $info in your discord server to learn
+#         more about this bot!
+
+#Invite link: https://discord.com/api/oauth2/authorize?client_id=862345408718307378&permissions=37219392&scope=bot
